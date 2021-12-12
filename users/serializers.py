db4c7c774e0c44b834fad7e68513ba4edf1430f8
MashinaSSokom <skltan@mail.ru>
2021-12-12 21:44:33 +0800

    add serialazers

---
+++ users/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
+from .models import User
+
+
+class UserModelSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
