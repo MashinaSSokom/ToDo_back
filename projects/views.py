99062e863a81e224eb96245b716f853bfb96502f
MashinaSSokom <skltan@mail.ru>
2022-01-17 12:56:49 +0800

    add Project and TODO ModelViewsets

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProjectModelSerializer, TODOModelSerializer
+
+from .models import Project, TODO
+
 
 # Create your views here.
+
+
+class ProjectModelViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class TODOModelViewSet(ModelViewSet):
+    queryset = TODO.objects.all()
+    serializer_class = TODOModelSerializer
