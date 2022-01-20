c6fc5aa676ece3529f3f2d10086bfcb460e25fae
MashinaSSokom <skltan@mail.ru>
2022-01-20 21:56:27 +0800

    add TODOs page

---
+++ front/src/pages/TODOs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TODOs = () => {
+    return (
+        <div>
+            <h1>TODOs</h1>
+        </div>
+    );
+};
+
+export default TODOs;
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,8 +1,10 @@
 import Projects from "../pages/Projects";
 import Users from "../pages/Users";
+import TODOs from "../pages/TODOs";
 
 
 export const publicRoutes = [
     {path: '/projects', component: Projects},
-    {path: '/users', component: Users}
+    {path: '/users', component: Users},
+    {path: '/todos', component: TODOs}
 ]
