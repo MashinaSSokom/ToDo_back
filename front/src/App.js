b733ac79f4abe44c6cf3a7fb8324b967687a8160
MashinaSSokom <skltan@mail.ru>
2022-01-21 00:46:39 +0800

    fix imports

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import axios from "axios";
 
-import UsersList from "./components/UsersList";
-import Menu from "./components/Menu";
-import Footer from "./components/Footer";
+import Footer from "./components/UI/footer/Footer";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
-import Navbar from "./components/UI/Navbar";
+import Navbar from "./components/UI/navbar/Navbar";
 
 class App extends React.Component {
     constructor(prop) {
