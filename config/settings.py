ae04cf7ad0c7a6dfea1ba9b8c0b8999794ba7cdf
MashinaSSokom <skltan@mail.ru>
2022-01-24 20:03:31 +0800

    delete rest_dramework.authtoken app

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'corsheaders',
     'django_filters',
