8260b954138aa0ce1ace0477a99094e740ba9bf7
MashinaSSokom <skltan@mail.ru>
2022-01-25 00:34:42 +0800

    add login route and navbar link

--- front/src/components/UI/navbar/Navbar.jsx
+++ front/src/components/UI/navbar/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
     return (
         <div className='navbar'>
             <div className='navbar__links'>
+                <Link to="/login">Войти</Link>
                 <Link to="/users">Пользователи</Link>
                 <Link to="/projects">Проекты</Link>
                 <Link to="/todos">TODOs</Link>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,11 +2,13 @@ import Projects from "../pages/Projects";
 import Users from "../pages/Users";
 import TODOs from "../pages/TODOs";
 import ProjectDetail from "../pages/ProjectDetail";
+import Login from "../pages/Login";
 
 
 export const publicRoutes = [
     {path: '/projects', component: <Projects/>},
     {path: '/projects/:id', component: <ProjectDetail/>},
     {path: '/users', component: <Users/>},
-    {path: '/todos', component: <TODOs/>}
+    {path: '/todos', component: <TODOs/>},
+    {path: '/login', component: <Login/>}
 ]
