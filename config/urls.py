3aca2af6bd9b220fbfec5ac4340225906f2f943b
MashinaSSokom <skltan@mail.ru>
2022-01-24 20:01:40 +0800

    change url orlder

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,6 @@ import config.settings as config
 from users.views import UserViewSet
 from projects.views import ProjectModelViewSet, TODOModelViewSet
 
-
 router = SimpleRouter()
 
 if config.DEBUG:
@@ -18,11 +17,9 @@ router.register('projects', ProjectModelViewSet)
 router.register('todos', TODOModelViewSet)
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-if config.DEBUG:
-    urlpatterns.append(path('api-auth', include('rest_framework.urls')))
