15a9cc217a521766ec3306ef92eee7f6309eaa20
MashinaSSokom <skltan@mail.ru>
2022-01-25 02:23:49 +0800

    add AuthContext provider

--- front/src/App.js
+++ front/src/App.js
@@ -6,24 +6,40 @@ import Footer from "./components/UI/footer/Footer";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import Navbar from "./components/UI/navbar/Navbar";
+import {AuthContext} from "./context";
 
 class App extends React.Component {
     constructor(prop) {
         super(prop);
         this.state = {
-            'users': []
+            'users': [],
+            'isAuth': false,
         }
+        this.setIsAuth = this.setIsAuth.bind(this)
+    }
+
+    setIsAuth(value) {
+        this.setState({
+            'isAuth': value
+        })
     }
 
     render() {
         return (
-            <div className={'App'}>
-                <BrowserRouter>
-                    <Navbar/>
-                    <AppRouter/>
-                    <Footer/>
-                </BrowserRouter>
-            </div>
+            <AuthContext.Provider value={{
+                setIsAuth: this.setIsAuth,
+                isAuth: this.state.isAuth
+            }}>
+                <div className={'App'}>
+
+                    <BrowserRouter>
+                        <Navbar/>
+                        <AppRouter/>
+                        <Footer/>
+                    </BrowserRouter>
+
+                </div>
+            </AuthContext.Provider>
         )
     }
 }
