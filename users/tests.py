935361f0304ac217ba40279697b5f2ba515c5cc1
MashinaSSokom <skltan@mail.ru>
2022-01-27 00:58:57 +0800

    add admin_group with right permissions

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,5 @@
 from itertools import chain
-
+from .apps import AppConfig
 from django.db.models import Q
 from django.test import TestCase
 from rest_framework import status
@@ -19,30 +19,28 @@ class TestUserView(APITestCase):
     def setUp(self) -> None:
         # mixer.blend()
 
-        admin_perm = set(chain(
-            Permission.objects.filter(content_type__model='project'),
-            Permission.objects.filter(Q(content_type__model='todo') & Q(codename__startswith='add'))
-        ))
-        Group.objects.create(name='AdminGroup')
-        admin_group = Group.objects.get(pk=1)
-        admin_group.permissions.set(admin_perm)
+        projperm = Permission.objects.filter(Q(content_type__model='project'))
+        todoperm = Permission.objects.filter(Q(codename__regex=r'^(add|view)+'), Q(content_type__model='todo'))
+        admin_group, created = Group.objects.get_or_create(name='AdminGroup')
 
+        admin_group.permissions.set(chain(todoperm, projperm))
 
-        self.admin = User.objects.create(username='TestAdmin', email='test@admin.com', password='123',
-                                         role=User.ADMIN,
-                                         is_active=True, is_staff=True)
-        self.admin = User.objects.get(pk=1)
-
-        admin_group.user_set.add(self.admin)
-
-        print(User.objects.get(pk=1).groups)
-
+        print(admin_group.permissions.all())
+        # admin_group.permissions.add(*projperm)
+        # admin_group.permissions.add(*todoperm)
+        # self.admin = User.objects.create(username='TestAdmin', email='test@admin.com', password='123',
+        #                                  role=User.ADMIN,
+        #                                  is_active=True, is_staff=True)
+        # self.admin = User.objects.get(pk=1)
+        #
+        # admin_group.user_set.add(self.admin)
+        #
+        # print(User.objects.get(pk=1).groups)
 
         # self.developer = User.objects.create(username='TestDeveloper', email='test@developer.com', password='123',
         #                                      role=User.DEVELOPER, is_active=True, is_staff=True)
         # self.manager = User.objects.create(username='TestManager', email='test@manager.com', password='123',
         #                                    role=User.MANAGER, is_active=True, is_staff=True)
-        print(self.admin.)
 
     def test_get_list_by_admin(self):
         factory = APIRequestFactory()
