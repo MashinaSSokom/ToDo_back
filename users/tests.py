406c7ab14b028c3c7997f3b2c4de5bf89b01d6ba
MashinaSSokom <skltan@mail.ru>
2022-01-27 02:26:28 +0800

    fix set admin_group to admin_user

--- users/tests.py
+++ users/tests.py
@@ -18,24 +18,17 @@ class TestUserView(APITestCase):
 
     def setUp(self) -> None:
         # mixer.blend()
-
         projperm = Permission.objects.filter(Q(content_type__model='project'))
         todoperm = Permission.objects.filter(Q(codename__regex=r'^(add|view)+'), Q(content_type__model='todo'))
         admin_group, created = Group.objects.get_or_create(name='AdminGroup')
 
         admin_group.permissions.set(chain(todoperm, projperm))
+        user = User.objects.create(username='TestAdmin', password='123', email='test@admin.com',
+                                   role=User.ADMIN,
+                                   is_active=True, is_staff=True)
+        self.admin = user
 
-        print(admin_group.permissions.all())
-        # admin_group.permissions.add(*projperm)
-        # admin_group.permissions.add(*todoperm)
-        # self.admin = User.objects.create(username='TestAdmin', email='test@admin.com', password='123',
-        #                                  role=User.ADMIN,
-        #                                  is_active=True, is_staff=True)
-        # self.admin = User.objects.get(pk=1)
-        #
-        # admin_group.user_set.add(self.admin)
-        #
-        # print(User.objects.get(pk=1).groups)
+        self.admin.groups.add(admin_group)
 
         # self.developer = User.objects.create(username='TestDeveloper', email='test@developer.com', password='123',
         #                                      role=User.DEVELOPER, is_active=True, is_staff=True)
@@ -44,8 +37,8 @@ class TestUserView(APITestCase):
 
     def test_get_list_by_admin(self):
         factory = APIRequestFactory()
+        print(self.admin.groups.all())
         self.client.force_login(self.admin)
         response = self.client.get('/api/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
         self.assertEqual(response.data.__len__(), 1)
