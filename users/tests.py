4da3e163ac47e303eb134869ba0f40266e2aa16e
MashinaSSokom <skltan@mail.ru>
2022-01-27 02:32:45 +0800

    fix admin group perms

--- users/tests.py
+++ users/tests.py
@@ -18,22 +18,24 @@ class TestUserView(APITestCase):
 
     def setUp(self) -> None:
         # mixer.blend()
-        projperm = Permission.objects.filter(Q(content_type__model='project'))
-        todoperm = Permission.objects.filter(Q(codename__regex=r'^(add|view)+'), Q(content_type__model='todo'))
-        admin_group, created = Group.objects.get_or_create(name='AdminGroup')
 
-        admin_group.permissions.set(chain(todoperm, projperm))
-        user = User.objects.create(username='TestAdmin', password='123', email='test@admin.com',
-                                   role=User.ADMIN,
-                                   is_active=True, is_staff=True)
-        self.admin = user
+        admin_perm = Permission.objects.all()
+        admin_group, created = Group.objects.get_or_create(name='AdminGroup')
+        admin_group.permissions.set(admin_perm)
+
+        self.admin = User.objects.create(username='TestAdmin', password='123', email='test@admin.com',
+                                         role=User.ADMIN,
+                                         is_active=True, is_staff=True)
 
         self.admin.groups.add(admin_group)
 
-        # self.developer = User.objects.create(username='TestDeveloper', email='test@developer.com', password='123',
-        #                                      role=User.DEVELOPER, is_active=True, is_staff=True)
-        # self.manager = User.objects.create(username='TestManager', email='test@manager.com', password='123',
-        #                                    role=User.MANAGER, is_active=True, is_staff=True)
+
+        # projperm = Permission.objects.filter(Q(content_type__model='project'))
+        # todoperm = Permission.objects.filter(Q(codename__regex=r'^(add|view)+'), Q(content_type__model='todo'))
+        self.developer = User.objects.create(username='TestDeveloper', email='test@developer.com', password='123',
+                                             role=User.DEVELOPER, is_active=True, is_staff=True)
+        self.manager = User.objects.create(username='TestManager', email='test@manager.com', password='123',
+                                           role=User.MANAGER, is_active=True, is_staff=True)
 
     def test_get_user_list_by_admin(self):
         self.client.force_login(self.admin)
